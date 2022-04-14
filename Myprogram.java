c3a19bc1c437afd86c2aabac2fd92b7b4f756619
itshivam242 <shivammittal242@gmail.com>
2022-04-14 09:43:30 +0530

    First Java Prog

---
+++ Myprogram.java
@@ -0,0 +1,11 @@
+class Myprogram{
+public static void main(String[] args){
+int room[][]={{101,102,103},{104,105,106}};
+for(int i=0;i<room.length;i++)
+for(int j=0;j<room[0].length;j++)
+{
+System.out.print(room[i][j]);
+System.out.print(" ");
+}
+}
+}
